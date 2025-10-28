4063167d1f2eaddafc57f8835f2235d46279515b
Julia <juliafarganus@gmail.com>
2025-10-28 21:39:06 +0100

    epic dislocated

--- explain_epic.py
+++ explain_epic.py
@@ -15,7 +15,7 @@ from einops import rearrange
 from plyfile import PlyData
 import torch.nn.functional as F
 
-def topk_active_channels(pointnet_model, ply_path, ds, topk, device):
+def topk_active_channels(epic_trainer, ply_path, ds, topk, device):
     data = load_and_preprocess_ply(ply_path)
     ids = ds._random_sample(data["pts"])
     features = data["gauss"][ids].unsqueeze(0).transpose(1, 2).to(device)
@@ -23,13 +23,19 @@ def topk_active_channels(pointnet_model, ply_path, ds, topk, device):
     mask = data.get("mask", None)
     if mask is not None:
         mask = mask[ids].to(device)
+    print(f"{features.device=}")
 
-    voxel_features, _ = pointnet_model.extract_voxel_features(features, xyz_normalized, mask)
-    voxel_features_flat = rearrange(voxel_features, 'b c x y z -> b c (x y z)')
+    with torch.no_grad():
+        point_features, xyz_for_vox = epic_trainer.pointnet.extract_point_features(features, xyz_normalized, mask)
+        voxel_features, indices_vox, point_counts = epic_trainer.pointnet.voxel_agg(point_features, xyz_for_vox, mask)
+
+    voxel_features_flat = voxel_features.view(voxel_features.size(0), voxel_features.size(1), -1)
+    voxel_features_flat = epic_trainer.epic(voxel_features_flat)
+    voxel_features_flat = F.relu(voxel_features_flat)
     voxel_features_flat = voxel_features_flat.squeeze(0)
-    voxel_features_flat = F.relu(voxel_features_flat) # C, V
-
+    
     max_voxel_activations, _ = torch.max(voxel_features_flat, dim=-1) # C
+    
     _, channels = torch.topk(max_voxel_activations, topk)
     return channels.tolist()
 
@@ -40,7 +46,7 @@ def explain_prediction(epic_trainer, ply_path, ds, topk, device):
         print("No stored prototypes found")
         prototypes = {}
 
-    channels = topk_active_channels(epic_trainer.pointnet, ply_path, ds, topk, device)
+    channels = topk_active_channels(epic_trainer, ply_path, ds, topk, device)
     print(f"Max active channels are {channels}")
     new_prototypes = {c: [-1] for c in channels}
     for c in channels:
@@ -69,13 +75,14 @@ def save_inference_stats(info, filename):
 
 def main(args):
     ply_path = args.ply_path
-    pointnet_ckpt = "checkpoints/pointnet_epic_compensated_grid_10.pt"
+    pointnet_ckpt = "checkpoints/toys_pointnet_epic_dislocated_10x10/pointnet_epic_compensated.pt"
     grid_size = 10
-    data_dir = "toys_ds_cleaned/train/"
+    data_dir = args.data_dir
     batch_size = 4
     num_workers = 2
     sampling = "random"
     num_samples = 17500
+    save_viz = args.save_viz
     num_prototypes = args.num_prototypes
     output_dir = args.output_path
     output_dir = os.path.join(output_dir, Path(ply_path).stem)
@@ -114,7 +121,10 @@ def main(args):
     epic_trainer.hparams.batch_size = batch_size
     epic_trainer.hparams.num_workers = num_workers
 
+
     device = "cuda" if torch.cuda.is_available() else "cpu"
+    epic_trainer.to(device=device)
+
 
     loader = DataLoader(
         dataset,
@@ -145,7 +155,8 @@ def main(args):
     )
 
     explain_prediction(epic_trainer, ply_path, ds=dataset, topk=num_prototypes, device=device)
-    epic_viz_cb.visualize_epic_prototypes(None, epic_trainer, is_first_explained=True)
+    if save_viz:
+        epic_viz_cb.visualize_epic_prototypes(None, epic_trainer, is_first_explained=True)
     stats = get_inference_stats(epic_trainer.last_val_prototypes, dataset)
     save_inference_stats(stats, os.path.join(output_dir, "inference_stats.json"))
 
@@ -155,5 +166,10 @@ if __name__ == "__main__":
     parser.add_argument('--ply_path', type=str, required=True, help='path of input ply file')
     parser.add_argument('--output_path', type=str, default="./epic-visualization/", help='path of output directory')
     parser.add_argument('--num_prototypes', type=int, default=5, help='number of prototypes to use')
+    parser.add_argument('--data_dir', type=str, default='./data/toys_ds_cleaned/train', help='directory of samples to choose from')
+    parser.add_argument('--save_viz', action='store_true', default=False, help='Save point cloud visualizations')
+    # parser.add_argument('--ply_path', type=str, default='./toys_ds_cleaned/test/9/sofa_000.ply', help='path of input ply file')
+    # parser.add_argument('--output_path', type=str, default="./epic-visualization-test/", help='path of output directory')
+    # parser.add_argument('--num_prototypes', type=int, default=5, help='number of prototypes to use')
     args = parser.parse_args()
     main(args)
