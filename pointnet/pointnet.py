bef7657e6ff439a755aebf962b902078ed766dc3
Dominik Galus <dominikgalus9@gmail.com>
2025-11-04 19:32:25 +0100

    feat: delete code

--- pointnet/pointnet.py
+++ pointnet/pointnet.py
@@ -122,7 +122,6 @@ class PointNetCls(nn.Module):
         super().__init__()
         self.grid_size = grid_size
 
-        # jeśli używamy stn, wyrzucamy inne cechy za xyz
         self.stn_3d = STN(in_dim=3, head_norm=stn_head_norm) if stn_3d else nn.Identity()
 
         self.conv1 = nn.Sequential(
